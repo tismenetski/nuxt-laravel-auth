b47acb3a21c83c11ccb41c03f99cfeac78255bd7
tismenetski <tismenetski@gmail.com>
2022-01-29 14:08:34 +0200

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,82 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'client',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    '@nuxtjs/auth-next'
+  ],
+
+  auth: {
+    strategies: {
+      laravelSanctum: {
+        provider: 'laravel/sanctum',
+        url: 'http://localhost:8000',
+        // url: 'http://laravel-nuxt-api.test',
+        endpoints : {
+          login : {
+                url : '/api/login'
+          },
+          logout : {
+            url : '/api/login'
+          },
+          user : {
+            url : '/api/user'
+          },
+        },
+        user : {
+          property : false
+        }
+      },
+    },
+    redirect: {
+      login : '/auth/login',
+      logout : '/',
+      home : '/dashboard'
+    },
+  },
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    baseURL: 'http://localhost:8000/',
+    credentials : true
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex flex-col justify-between min-h-screen">
+    <div class="flex-1 flex flex-col justify-center items-start space-y-4 ml-10">
+      <h1 class="text-white text-3xl">
+        Welcome to Coder's Conduct
+      </h1>
+      <div class="flex space-x-12">
+        <NuxtLink to="/auth/register" class="text-white text-2xl hover:text-blue-500">
+          REGISTER
+        </NuxtLink>
+        <NuxtLink to="/auth/login" class="text-white text-2xl hover:text-blue-500">
+          LOGIN
+        </NuxtLink>
+      </div>
+    </div>
+    <div class="flex-1 flex items-end mb-10 ml-10">
+      <p class="text-white">
+        Photo by <a href="https://unsplash.com/@romulusprince?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" class="text-blue-500 hover:text-white">Wajih Ghali</a> on <a href="https://unsplash.com/@romulusprince?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" class="text-blue-500 hover:text-white">Unsplash</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
